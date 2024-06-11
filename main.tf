a87a91b59733b15cc9f54d8f20c96563cfd13bed
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-06-11 12:32:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-1b"
   tags = {
     Name = "subnet"
   }
