f36868a5002b6f20ee95f1ec9a3c4d36f45a2fea
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-06-11 12:47:00 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum install git -y
-sudo git clone https://github.com/kaasu-pavani/flight-prediction.git
+sudo git clone https://github.com/kaasu-pavani/USA-Housing.git
 cd /
-cd flight-prediction/
+cd USA-Housing
 sudo yum install python3 python3-pip -y
 pip3 install -r requirements.txt
 screen -m -d python3 app.py 
