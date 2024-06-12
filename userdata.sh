35962b9e03a37ea175e0b02527385ce441e1c700
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-06-12 09:57:18 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 sudo yum install git -y
 sudo git clone https://github.com/kaasu-pavani/car-prediction.git
 cd /
-cd car-prediction
+cd car-prediction/
 sudo yum install python3 python3-pip -y
 pip3 install -r requirements.txt
 screen -m -d python3 app.py 
